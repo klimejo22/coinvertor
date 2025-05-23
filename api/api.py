c0cb7a683bec0af7baac0d431dc30256d4bf2f53
josefkliment <klimejo22@sps-prosek.cz>
2025-05-23 14:50:44 +0200

    Konecne zprovozneni databazove sluzby

--- api/api.py
+++ api/api.py
@@ -29,7 +29,7 @@ def convert_currency(c1: str, c2: str, val: float):
     # c2: Druha mena
     # val: Pocet meny
 
-    with engine.begin() as connection:
+    with engine.connect() as connection:
         try:
             base_id = connection.execute(
                 text("SELECT id FROM currencies WHERE name = :name"),
@@ -42,81 +42,65 @@ def convert_currency(c1: str, c2: str, val: float):
         except SQLAlchemyError as e:
             return raise_db_error(e, 40)
         if id_check_result.first() is None:
-            if opendata_fail(add_data(c1)):
-                return
-        else:
-            query = text("""
-                SELECT 
-                    er.rate
-                FROM exchange_rates er
-                JOIN currencies c1 ON er.base_currency_id = c1.id
-                JOIN currencies c2 ON er.target_currency_id = c2.id
-                WHERE c1.name = :base_currency_name
-                AND c2.name = :target_currency_name;
-            """)
-            
-            try:
-                result = connection.execute(query, {"base_currency_name": c1, "target_currency_name": c2}).scalar()
-            except SQLAlchemyError as e:
-                return raise_db_error(e, 58)
+            add_result = add_data(c1)
+            if add_result["Result"] != "Success":
+                return raise_db_error(add_result["Result"] + "na radku" + str(add_result["Line"]), 46)
+        
+        query = text("""
+            SELECT 
+                er.rate
+            FROM exchange_rates er
+            JOIN currencies c1 ON er.base_currency_id = c1.id
+            JOIN currencies c2 ON er.target_currency_id = c2.id
+            WHERE c1.name = :base_currency_name
+            AND c2.name = :target_currency_name;
+        """)
+        
+        try:
+            result = connection.execute(query, {"base_currency_name": c1, "target_currency_name": c2}).scalar()
+        except SQLAlchemyError as e:
+            return raise_db_error(e, 58)
 
-            return {
-                "Result": "success",
-                "c1_val": val,
-                "c2_val": result * val
-            }
+    return {
+        "Result": "success",
+        "c1_val": val,
+        "c2_val": result * val
+    }
     
 @app.get("/addData/{input_currency}")
 def add_data(input_currency: str):
-    
-    query = text("""
-    SELECT 
-        c1.name AS base_currency, 
-        c2.name AS target_currency, 
-        er.rate
-    FROM exchange_rates er
-    JOIN currencies c1 ON er.base_currency_id = c1.id
-    JOIN currencies c2 ON er.target_currency_id = c2.id
-    WHERE c1.name = :input_currency;
-    """)
 
-    with engine.begin() as connection:
+    with engine.connect() as connection:
 
-        try: 
-            result = connection.execute(query, {"input_currency": input_currency})
-        except SQLAlchemyError as e:
-            return raise_db_error(e, 85)
-        
-        if result.first() is None:
-            response = requests.get(baseUrl + input_currency).json()
-            if opendata_fail(response):
-                return {"Result": "Currency API failure"}
-            else:
+        response = requests.get(baseUrl + input_currency).json()
+        if opendata_fail(response):
+            return {"Result": "Currency API failure"}
+        else:
 
+            try:
+                base_id = connection.execute(
+                    text("SELECT id FROM currencies WHERE name = :name"),
+                    {"name": input_currency}
+                ).scalar()
+            except SQLAlchemyError as e:
+                return raise_db_error(e, 96)
+
+            for key, value in response["rates"].items():
                 try:
-                    base_id = connection.execute("SELECT id FROM currencies WHERE name = :name", {"name" : input_currency}).scalar()
+                    target_id = connection.execute(
+                        text("SELECT id FROM currencies WHERE name = :name"),
+                        {"name": key}
+                    ).scalar()
+                    connection.execute(
+                        text("INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate) VALUES (:base_id, :target_id, :rate)"),
+                        {
+                            "base_id": base_id,
+                            "target_id": target_id,
+                            "rate": value
+                        }
+                    )
                 except SQLAlchemyError as e:
-                    return raise_db_error(e, 96)
+                    return raise_db_error(e, 107)
+            connection.commit()
 
-                for key, value in response["rates"].items():
-                    try:
-                        target_id = connection.execute("SELECT id FROM currencies WHERE name = :name", {"name" : key}).scalar()
-                        connection.execute("INSERT INTO exchange_rates (base_currency_id, target_currency_id, rate) VALUES (:base_id, :target_id, :rate)", {
-                        "base_id": base_id,
-                        "target_id": target_id,
-                        "rate": value
-                        })
-                    except SQLAlchemyError as e:
-                        return raise_db_error(e, 107)
-                # connection.commit()
-        #         add_data(input_currency)
-        # else:
-
-        #     data = []
-        #     for row in result:
-        #         data.append({
-        #             "base_currency": row.base_currency,
-        #             "target_currency": row.target_currency,
-        #             "rate": row.rate
-        #         })
     return {"Result": "Success"}
